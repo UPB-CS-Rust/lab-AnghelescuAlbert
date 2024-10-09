64bf3aba0b5bf0ca7215cc8b0f4d65983a486d64
AnghelescuAlbert <albert.anghelescu04@gmail.com>
2024-10-09 18:51:39 +0300

    Lab1

--- 2-foundations-of-rust/1-basic-syntax/1-basic-syntax/src/bin/03.rs
+++ 2-foundations-of-rust/1-basic-syntax/1-basic-syntax/src/bin/03.rs
@@ -1,7 +1,16 @@
 fn main() {
     let input = [23, 82, 16, 45, 21, 94, 12, 34];
 
+    let mut max = input[0];
+    let mut min = input[0];
     // TODO
-
-    println!("{} is largest and {} is smallest");
+    for i in 1..7 {
+        if max < input[i] {
+            max = input[i];
+        }
+        if min > input[i] {
+            min = input[i];
+        }
+    }
+    println!("{} is largest and {} is smallest", max, min);
 }
